890db60b67cdcb7313b0763dbfdfc7f0f8b01f8c
Attenna <george_5298@BUPT.edu.cn>
2025-07-30 12:15:09 +0800

    Initial commit

---
+++ cpp5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1 = 8, t1 = 30, n2 = 10, t2 = 6;
+    int t3 = 10;
+    double inc_rete = (1.0 * n1*t1 -n2*t2 ) / (t1-t2);
+    double init_num = n2 * t2 - inc_rete *t2;
+    printf("%f %f", inc_rete, init_num);
+}
